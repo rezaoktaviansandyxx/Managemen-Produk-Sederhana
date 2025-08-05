03108322f4bffe31b8a7b4f8c5b38d99584e0b73
unknown <indraokisandy1@gmail.com>
2025-08-05 12:08:45 +0700

    fixing edit product

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -1,31 +1,27 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Product_model extends CI_Model
-{
-    public function get_all()
-    {
-        return $this->db->order_by('created_at', 'DESC')->get('products')->result();
+class Product_model extends CI_Model {
+    public function get_all($search = '') {
+        if ($search) {
+            $this->db->like('name', $search);
+        }
+        return $this->db->get('products')->result();
     }
 
-    public function get($id)
-    {
+    public function get_by_id($id) {
         return $this->db->get_where('products', ['id' => $id])->row();
     }
 
-    public function insert($data)
-    {
-        $data['created_at'] = date('Y-m-d H:i:s');
+    public function insert($data) {
         return $this->db->insert('products', $data);
     }
 
-    public function update($id, $data)
-    {
-        return $this->db->where('id', $id)->update('products', $data);
+    public function update($id, $data) {
+        return $this->db->update('products', $data, ['id' => $id]);
     }
 
-    public function delete($id)
-    {
-        return $this->db->where('id', $id)->delete('products');
+    public function delete($id) {
+        return $this->db->delete('products', ['id' => $id]);
     }
 }
